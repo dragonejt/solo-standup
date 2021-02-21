beaf52b47724eecf4b6eb9d4fd6642f93b776048
Evan Tung <dragonejt@gmail.com>
2021-02-20 17:49:48 -0800

    FInish Landing and Add Progress page

--- src/components/progress/Progress.js
+++ src/components/progress/Progress.js
@@ -1,30 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Button, Jumbotron } from 'react-bootstrap';
+import { Container, Button, Jumbotron, Form, FormGroup, FormControl, FormLabel } from 'react-bootstrap';
 import './progress.css'
 
 
 function Progress({ updateStep }) {
 
-  useEffect(() => {
-    updateStep(1);
-  }
+    useEffect(() => {
+        updateStep(1);
+    }
 
-  )
-  return (
-    <div className="Progress">
-      <Container>
-        <Jumbotron>
-          <h1 className="title">What have you accomplished between the last standup meeting and now?</h1>
-          <br className="my-3"/>
-          <p className="lead subtitle">Like having a standup meeting, but just with yourself</p>
-          <br className="my-3"/>
-          <p>
-            <Button variant="primary" size="lg" href="/progress" id="submit">Next -></Button>
-          </p>
-        </Jumbotron>
-      </Container>
-    </div>
-  );
+    )
+    return (
+        <div className="Progress">
+            <Container>
+                <Jumbotron>
+                    <h1 className="question">What have you accomplished between the last standup meeting and now?</h1>
+                    <br className="my-3" />
+                    <Form.Group controlId="ProgressCheck">
+                        <Form.Control as="textarea" class="answer"/>
+                    </Form.Group>
+                    <br className="my-3" />
+                    <p>
+                        <Button variant="primary" size="lg" href="/progress" id="submit">Next -></Button>
+                    </p>
+                </Jumbotron>
+            </Container>
+        </div>
+    );
 }
 
 export default Progress;
